8ab0bee485b59119d47459878e619df4981601e1
joseph-beep <74767414+joseph-beep@users.noreply.github.com>
2021-04-12 11:53:53 +0200

    Delete oude main.py bestand

--- main.py
+++
@@ -1,9 +0,0 @@
-import random
-from bke import EvaluationAgent, start
-
-class RandomAgent(EvaluationAgent):
-    def evaluate(self, board, my_symbol, opponent_symbol):
-        return random.randint(1, 500)
-
-random_agent = RandomAgent()
-start(player_o=random_agent)
